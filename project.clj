f2af300ecf855668380ae8e64435cc1b094e05a0
Christopher Mark Gore <cgore@cgore.com>
2014-02-12 18:22:25 -0600

    Working hello world.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject foo "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :main  ^:skip-aot foo.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
--- src/foo/core.clj
+++ src/foo/core.clj
@@ -1,1 +1,4 @@
 (ns foo.core)
+
+(defn -main [& args]
+  (println "I'm a little teapot!"))
