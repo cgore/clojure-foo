b8578547ca1d52249d8024996528cff1ea8954c7
Christopher Mark Gore <cgore@cgore.com>
2014-02-12 21:01:41 -0600

    I like AOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject foo "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main  ^:skip-aot foo.core
+  :main foo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
